c134047cd619bd1cc64e759789eb1023b4d13a7a
SlavBrain <82366112+SlavBrain@users.noreply.github.com>
2022-12-01 14:14:57 +0300

    add player movement(need fix)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private const float _gravity=-9.8f;
+
+    [SerializeField]private float _horizontalSpeed=1f;
+    [SerializeField] private float _jumpHeight = 1f;
+    [SerializeField] private float _jumpSpeed = 5f;
+    [SerializeField] private float _groundCheckRadius=0.1f;
+    [SerializeField] private LayerMask _groundLayer;
+    [SerializeField]private float _verticalSpeed = 0f;
+
+    [SerializeField]private bool _onGround = false;
+
+    private void Update()
     {
-        
+        if (IsOnGround())
+        {
+            _verticalSpeed = 0;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+            }
+        }
+        else
+        {
+            Fall();
+        }
+        MoveForward();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MoveForward()
     {
-        
+        transform.Translate(new Vector3(_horizontalSpeed * Time.deltaTime, _verticalSpeed*Time.deltaTime, 0));
     }
+
+    private void Jump()
+    {
+        _verticalSpeed = _jumpSpeed +Mathf.Sqrt(_jumpHeight / (2 * _gravity * _gravity));
+    }
+
+    private void Fall()
+    {
+        _verticalSpeed += _gravity * Time.deltaTime;
+    }
+
+    private bool IsOnGround()
+    {
+        return _onGround= Physics.OverlapSphere(transform.position, _groundCheckRadius, _groundLayer).Length != 0;
+    }
+
 }
